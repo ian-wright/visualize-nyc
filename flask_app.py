6a7edb2e8d6cd84c40811e37281d972054d469f7
wrightangles <ian.f.t.wright@gmail.com>
2017-01-22 17:54:21 -0500

    atrain link

--- flask_app.py
+++ flask_app.py
@@ -6,13 +6,20 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-comments = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html", comments=comments)
 
+    # otherwise, it's a POST
     comments.append(request.form["contents"])
     return redirect(url_for('index'))
 
+
+@app.route("/atrain/")
+def atrain():
+	return render_template("atrain.html")
+
+if __name__ = '__main__':
+    app.run()
