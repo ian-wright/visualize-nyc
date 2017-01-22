ababe65e854240524cfefc11f70c808a41a69692
wrightangles <ian.f.t.wright@gmail.com>
2017-01-22 18:03:40 -0500

    flask app

--- flask_app.py
+++ flask_app.py
@@ -9,17 +9,13 @@ app.config["DEBUG"] = True
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "GET":
-        return render_template("index.html", comments=comments)
-
-    # otherwise, it's a POST
-    comments.append(request.form["contents"])
-    return redirect(url_for('index'))
+    return render_template("index.html")
 
 
 @app.route("/atrain/")
 def atrain():
 	return render_template("atrain.html")
+
 
 if __name__ = '__main__':
     app.run()
